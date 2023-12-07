c7114c1ac30d45d55d3d47600a24533038fafa29
O-keita <o.keita@alustudent.com>
2023-12-07 19:27:34 +0200

    access granted

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,21 @@
-Welcome to Nzavote voting system!\nwe make election processes easy\nand fraud-free!
+    connection = mysql.connector.connect(**db_config)
+
+    # Create a cursor to execute SQL queries
+    cursor = connection.cursor()
+
+    try:
+        # Create a user with a wildcard for username and hostname
+        cursor.execute("CREATE USER '%'@'%' IDENTIFIED BY 'password';")
+
+        # Grant all privileges on all databases and tables
+        cursor.execute("GRANT ALL PRIVILEGES ON *.* TO '%'@'%';")
+
+        # Apply changes
+        connection.commit()
+        print("Access granted to everyone.")
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+    finally:
+        # Close the cursor and connection
+        cursor.close()
+        connection.close()
